49f1839ab0dab616ff6fec00d555b4117c4f6358
roeenangelo <roeenangelo@gmail.com>
2026-01-20 01:22:41 +0800

    Add sprites

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from circleshape import CircleShape
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
+    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill((0, 0, 0)) # Clear screen with black
-    screen.display.flip() # Refresh the screen
-    
+        screen.fill((0, 0, 0)) # Clear screen with black
+        pygame.display.flip() # Refresh the screen
+        clock.tick(60) # Cap the frame rate at 60 FPS
+        dt = clock.tick() / 1000.0 # Delta time in seconds
+        
 
 if __name__ == "__main__":
     main()
